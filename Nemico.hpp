78a6f3453edce676f3484b9b854e182c3b15c2ca
giorgiadpaf <165424525+giorgiadpaf@users.noreply.github.com>
2026-05-14 11:58:42 +0200

    Update Nemico.hpp

--- Nemico.hpp
+++ Nemico.hpp
@@ -7,14 +7,14 @@ class Nemico : public Entita {
 protected:
     bool vivo; // 1 se è in gioco, 0 se è stato colpito
     int tipo; // 1 = Casuale, 2 = Inseguitore (o veloce)
-    int frameDelay; // Per gestire la velocità (Pagina 16: "alcuni più veloci")
+    int frameDelay; // Per gestire la velocità
     int frameCounter;
 
 public:
     Nemico(int y, int x, Mappa* m, int t);
     int movimento(int pY, int pX, int dirCorrente);
     void disegna();
-    void controllaMorte(); // Controlla se è nel fuoco
+    void controllaMorte(); 
     bool isVivo() const;
 };
 #endif
