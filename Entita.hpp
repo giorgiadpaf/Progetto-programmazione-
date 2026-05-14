b1504e7def6ea76e91fb7a9542b72f0b4cc839c0
giorgiadpaf <165424525+giorgiadpaf@users.noreply.github.com>
2026-05-14 12:02:36 +0200

    Update Entita.hpp

--- Entita.hpp
+++ Entita.hpp
@@ -7,17 +7,15 @@ class Mappa;
 class Entita {
 protected:
     int y, x;      // Coordinate attuali
-    char simbolo;  // Il carattere ASCII (es. '@')
+    char simbolo;  // Il carattere ASCII 
     Mappa* punmappa;
 
 public:
-    // Costruttore: decide dove parte il giocatore
+
     Entita(int startY, int startX, Mappa* mappa,  char sym);
 
-    // Funzione per stampare il giocatore sullo schermo
     virtual void disegna();
 
-    // Getter per le coordinate (serviranno agli altri componenti)
     int getY() const;
     int getX() const;
 };
